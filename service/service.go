c0dda4a8f2af7f43256db5ba98e1a17178413495
heeg <heeg@qq.com>
2021-06-04 06:16:21 +0800

    fix: remove plugin

--- service/service.go
+++ service/service.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/asim/go-micro/plugins/wrapper/breaker/hystrix/v3"
-	ratelimiter "github.com/asim/go-micro/plugins/wrapper/ratelimiter/ratelimit/v3"
+	// "github.com/asim/go-micro/plugins/wrapper/breaker/hystrix/v3"
+	// ratelimiter "github.com/asim/go-micro/plugins/wrapper/ratelimiter/ratelimit/v3"
 
 	grpc "github.com/asim/go-micro/plugins/transport/grpc/v3"
 
@@ -18,7 +18,7 @@ import (
 	"github.com/asim/go-micro/v3"
 	"github.com/asim/go-micro/v3/client"
 	"github.com/asim/go-micro/v3/server"
-	"github.com/juju/ratelimit"
+	// "github.com/juju/ratelimit"
 
 	"github.com/micro/go-micro/v2/config"
 )
@@ -47,7 +47,7 @@ func logWrapper(fn server.HandlerFunc) server.HandlerFunc {
 func NewService() micro.Service {
 	// Create a new service. Optionally include some options here.
 	// 设置限流，设置能同时处理的请求数，超过这个数就不继续处理
-	br := ratelimit.NewBucketWithRate(float64(config.Get("rate").Int(1000)), int64(config.Get("rate").Int(1000)+200))
+	// br := ratelimit.NewBucketWithRate(float64(config.Get("rate").Int(1000)), int64(config.Get("rate").Int(1000)+200))
 
 	regis := s2s.NewRegistry(registry.Addrs(config.Get("s2s", "address").String("")))
 	svr := micro.NewService(
@@ -64,13 +64,13 @@ func NewService() micro.Service {
 		// doc:https://medium.com/@dche423/micro-in-action-7-cn-ce75d5847ef4
 		// 熔断功能作用于客户端，设置恰当阈值以后， 它可以保障客户端资源不会被耗尽
 		// —— 哪怕是它所依赖的服务处于不健康的状态，也会快速返回错误，而不是让调用方长时间等待。
-		micro.WrapClient(hystrix.NewClientWrapper()),
+		// micro.WrapClient(hystrix.NewClientWrapper()),
 		// 用于限流限频
 		// 与熔断类似， 限流也是分布式系统中常用的功能。
 		// 不同的是， 限流在服务端生效，它的作用是保护服务器： 在请求处理速度达到设定的限制以后，
 		// 便不再接收和处理更多新请求，直到原有请求处理完成， 腾出空闲。 避免服务器因为客户端的疯狂调用而整体垮掉。
-		micro.WrapClient(ratelimiter.NewClientWrapper(br, false)),
-		micro.WrapHandler(ratelimiter.NewHandlerWrapper(br, false)),
+		// micro.WrapClient(ratelimiter.NewClientWrapper(br, false)),
+		// micro.WrapHandler(ratelimiter.NewHandlerWrapper(br, false)),
 
 		// 客户端调用跟踪，每个请求调用之前都会调用这个中间件函数
 		micro.WrapCall(metricsWrap),
