8c0ee3218961f2b329a7c5401a1acd507a5b5795
dataspace <dataspace@126.com>
2021-10-18 08:51:44 +0800

    fix: add NewServiceNoMetrics

--- service/service.go
+++ service/service.go
@@ -149,27 +149,9 @@ func logWrapper(fn server.HandlerFunc) server.HandlerFunc {
 
 // 获取客户端对象
 //
-// func NewClient() client.Client {
-// svr := NewService()
-// return svr.Client()
-// }
-
-// 获取http客户端对象
-//
-// @return Client
-//
 func NewClient() client.Client {
-	regis := s2s.NewRegistry(
-		registry.Addrs(config.Get("s2s", "address").String("")),
-		registry.Secure(config.Get("s2s", "secure").Bool(false)),
-	)
-
-	c := client.NewClient(
-		client.Registry(regis),
-	)
-	c.Options().Selector.Init(selector.Registry(regis))
-
-	return c
+	svr := NewService()
+	return svr.Client()
 }
 
 // 获取服务对象
@@ -212,6 +194,65 @@ func NewService() micro.Service {
 		micro.WrapCall(metricsWrap),
 		// 服务端被调跟踪，每个请求被处理之前都会调用这个中间件函数
 		micro.WrapHandler(logWrapper),
+		micro.BeforeStop(func() error {
+			if nil == s2s.GetDeregister().LocalSvr {
+				return nil
+			}
+
+			regis.Deregister(s2s.GetDeregister().LocalSvr)
+
+			// 等待3秒结束
+			logger.Info("Waitting 3 seconed over!")
+			for i := 0; i < 3; i++ {
+				logger.Info("Waitting 3 seconed over! ........ ", 3-i)
+				time.Sleep(1 * time.Second)
+			}
+
+			return nil
+		}),
+	)
+
+	svr.Init()
+	return svr
+}
+
+
+// 获取没有上报metrics的服务对象
+//
+func NewServiceNoMetrics() micro.Service {
+	svr_name = config.Get("name").String("")
+	hystrixsrc.DefaultTimeout = config.Get("timeout").Int(3) * 1000
+
+	// Create a new service. Optionally include some options here.
+	// 设置限流，设置能同时处理的请求数，超过这个数就不继续处理
+	br := ratelimit.NewBucketWithRate(float64(config.Get("rate").Int(1000)), int64(config.Get("rate").Int(1000)+200))
+
+	regis := s2s.NewRegistry(
+		registry.Addrs(config.Get("s2s", "address").String("")),
+		registry.Secure(config.Get("s2s", "secure").Bool(false)),
+	)
+	svr := micro.NewService(
+		micro.Name(config.Get("name").String("")),
+		micro.Transport(grpc.NewTransport()),
+		micro.Registry(regis),
+		micro.Version(config.Get("version").String("0.0.1")),
+
+		// 设置熔断,超过默认值就直接不发送请求
+		// 可以通过 github.com/afex/hystrix-go/hystrix设置默认值
+		// 超时时间和并发数
+		// 所有从此节点发出的Micro服务调用都会受到熔断插件的限制和保护。
+		// 熔断是调用级别的
+		// doc:https://medium.com/@dche423/micro-in-action-7-cn-ce75d5847ef4
+		// 熔断功能作用于客户端，设置恰当阈值以后， 它可以保障客户端资源不会被耗尽
+		// —— 哪怕是它所依赖的服务处于不健康的状态，也会快速返回错误，而不是让调用方长时间等待。
+		micro.WrapClient(hystrix.NewClientWrapper()),
+		// 用于限流限频
+		// 与熔断类似， 限流也是分布式系统中常用的功能。
+		// 不同的是， 限流在服务端生效，它的作用是保护服务器： 在请求处理速度达到设定的限制以后，
+		// 便不再接收和处理更多新请求，直到原有请求处理完成， 腾出空闲。 避免服务器因为客户端的疯狂调用而整体垮掉。
+		micro.WrapClient(ratelimiter.NewClientWrapper(br, false)),
+		micro.WrapHandler(ratelimiter.NewHandlerWrapper(br, false)),
+
 		micro.BeforeStop(func() error {
 			if nil == s2s.GetDeregister().LocalSvr {
 				return nil
